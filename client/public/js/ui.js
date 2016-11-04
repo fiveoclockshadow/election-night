d9f15a1725215370a5888d0fa8af3e718928cf4e
Syed Ali <sali3@cox.net>
2016-11-04 17:06:31 -0400

    Added candidate list  and accomplish how to delete a candidate

--- client/public/js/ui.js
+++ client/public/js/ui.js
@@ -1,29 +1,30 @@
 (function(){
 
-"use strict";
+  "use strict";
 
-window.election = window.election || {};
-
-console.log("I am in ui.js");
+  window.election = window.election || {};
 
 
+  // console.log("I am in ui.js");
+
+  window.election.buildList = function buildList(candidate_data) {
+    candidate_data.forEach(function loopArray(candidate){
+      // console.log(obj);
+      $(".list-of-candidates ul")
+        .append(
+          '<li>' +
+           " Name:" + candidate.name +
+           " Charisma: "+ candidate.charisma +
+           " Willpower: " + candidate.willpower +
+           " Intelligence: " + candidate.intelligence +
+           " <a href='#' class='delete-candidate' data-candidate-id= '" + candidate.id + "'>Delete</a>"+
+          '</li>'
+        );
+    });
+  };
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+///Close of IIFE
 })();
